4baa4a3b15c1faf5dd3387c2b0e6684940d4d608
fantotsy <fantotsy@gmail.com>
2016-04-25 20:25:00 +0300

    SortedByFuelConsumption class added

---
+++ src/ua/fantotsy/car/comparator/SortedByFuelConsumption.java
@@ -0,0 +1,20 @@
+package ua.fantotsy.car.comparator;
+
+import ua.fantotsy.car.Vehicle;
+
+import java.util.Comparator;
+
+public class SortedByFuelConsumption implements Comparator<Vehicle>{
+    public int compare(Vehicle car1, Vehicle car2){
+        int fuelConsumption1 = car1.getFuelConsumption();
+        int fuelConsumption2 = car2.getFuelConsumption();
+
+        if(fuelConsumption1 < fuelConsumption2){
+            return -1;
+        }
+        if(fuelConsumption1 > fuelConsumption2){
+            return 1;
+        }
+        return 0;
+    }
+}
